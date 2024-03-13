b2a25b3e2dbd23d37710106591484be1b230de5a
Lysunenko, Stanislav <stanislav.lysunenko@sabre.com>
2024-03-13 14:53:39 +0100

    [feature] Added draft files for svg, js and html sections, added main controller

---
+++ src/main/java/com/university/shophub/frontend/controllers/MainController.java
@@ -0,0 +1,17 @@
+package com.university.shophub.frontend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String indexPage(Model model) { return "index"; }
+
+    @RequestMapping("/admin/")
+    public String adminPage(Model model) {
+        return "index";
+    }
+}
